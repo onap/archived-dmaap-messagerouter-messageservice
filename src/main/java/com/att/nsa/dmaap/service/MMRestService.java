2baf2d898c2dc62f4bd6b76c8a9da7506884b544
Soumendu Sekhar Acharya <sa00498080@techmahindra.com>
2017-12-12 17:06:44 +0530

    sonar critical for Exception handling
    Use a logger to log this exception
    Sonar Link:
    https://sonar.onap.org/component_issues/index?id=org.onap.dmaap.messagerouter.messageservice%3AdmaapMR1#assignees=soumendu|severities=CRITICAL|resolved=false
    Location:
    src/main/java/com/att/nsa/dmaap/service/MMRestService.java
    Line No-1011
    
    Change-Id: Ie43812c236ad539aa7fa3e1685ef28c9aa20397e
    Issue-ID: DMAAP-188
    Signed-off-by: Soumendu Sekhar Acharya <sa00498080@techmahindra.com>

--- src/main/java/com/att/nsa/dmaap/service/MMRestService.java
+++ src/main/java/com/att/nsa/dmaap/service/MMRestService.java
@@ -1006,7 +1006,6 @@ public class MMRestService {
 							inStream = IOUtils.toInputStream(listAll.toString(), UTF_8);
 
 						} catch (IOException ioe) {
-							ioe.printStackTrace();
 							LOGGER.error("IOException: ", ioe);
 						}
 						// call listAllMirrorMaker
