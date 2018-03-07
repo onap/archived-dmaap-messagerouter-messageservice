afd26e40bddd223c335416fe18fabb8445864495
Sunil Unnava <su622b@att.com>
2018-03-07 13:34:35 -0500

    testcases for code coverage
    Issue-ID: DMAAP-323
    Change-Id: I0c2fb3091e8719e7391b658059525f9661306414
    Signed-off-by: Sunil Unnava <su622b@att.com>

--- src/main/resources/docker-compose/download-kafka.sh
+++ src/main/resources/docker-compose/download-kafka.sh
@@ -21,6 +21,9 @@
 #  
 #*******************************************************************************
 
-mirror=$(curl --stderr /dev/null https://www.apache.org/dyn/closer.cgi\?as_json\=1 | jq -r '.preferred')
-url="${mirror}kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
+#mirror=$(curl --stderr /dev/null https://www.apache.org/dyn/closer.cgi\?as_json\=1 | jq -r '.preferred')
+#url="${mirror}kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
+#wget -q "${url}" -O "/tmp/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
+
+url="http://archive.apache.org/dist/kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
 wget -q "${url}" -O "/tmp/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
