6f31d8db0e6a2affbad74b669a2f05199fcd3f6b
sunil unnava <sunil.unnava@att.com>
2018-10-24 19:53:56 -0400

    update the package name
    Issue-ID: DMAAP-858
    Change-Id: Ia3b75487cc79dea0454f024c3c69a6c4a8291e44
    Signed-off-by: sunil unnava <sunil.unnava@att.com>

--- src/main/ajsc/dmaap_v1/dmaap/v1/conf/jaxrsBeans.groovy
+++ src/main/ajsc/dmaap_v1/dmaap/v1/conf/jaxrsBeans.groovy
@@ -5,7 +5,15 @@
 	
 	echoService(org.onap.dmaap.JaxrsEchoService)
 	userService(org.onap.dmaap.JaxrsUserService)
-		
+	topicService(org.onap.dmaap.service.TopicRestService)
+	eventService(org.onap.dmaap.service.EventsRestService)
+	adminServiceorg.onap.dmaap.service.AdminRestService)
+	apiKeyService(org.onap.dmaap.service.ApiKeysRestService)
+	metricsService(org.onap.dmaap.service.MetricsRestService)
+	transactionService(org.onap.dmaap.service.TransactionRestService)
+	UIService(org.onap.dmaap.service.UIRestServices)
+	mirrorService(org.onap.dmaap.service.MMRestService)
+	
 	util.list(id: 'jaxrsServices') {
 		ref(bean:'echoService')
 		ref(bean:'userService')
