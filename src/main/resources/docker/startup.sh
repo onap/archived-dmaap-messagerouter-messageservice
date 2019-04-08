dea99142a99458e5c12cdcb5d4116921115a3838
sunil.unnava <su622b@att.com>
2019-04-08 17:40:01 -0400

    run as non root user
    Issue-ID: DMAAP-1040
    Change-Id: Ib66cfc24a10194be986414af90dfa6d828351a95
    Signed-off-by: sunil.unnava <su622b@att.com>

--- src/main/resources/docker/startup.sh
+++ src/main/resources/docker/startup.sh
@@ -21,7 +21,7 @@
 #*******************************************************************************
 root_directory="/appl/${project.artifactId}"
 config_directory="/appl/${project.artifactId}/bundleconfig"
-runner_file="appl/${project.artifactId}/lib/ajsc-runner-${ajscRuntimeVersion}.jar"
+runner_file="/appl/${project.artifactId}/lib/ajsc-runner-${ajscRuntimeVersion}.jar"
 
 echo "AJSC HOME directory is " $root_directory
 echo "AJSC Conf Directory is" $config_directory
